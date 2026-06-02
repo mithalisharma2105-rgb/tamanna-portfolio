e172e88438dea33a9e38fb0a05a9bafdb29e5910
mithalisharma2105-rgb <mithalisharma2105@gmail.com>
2026-06-02 21:34:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/tamanna-portfolio/', 
+  base: '/tamanna-portfolio/',
   plugins: [react()],
 })
